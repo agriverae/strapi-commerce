8574c2072b9e58828ccbef44ce639bda4fab26b5
agriverae <ali.rivera.echegaray@gmail.com>
2021-08-19 16:47:28 -0300

    updated config

---
+++ config/env/production/database.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  connections: {
+    default: {
+      settings: {
+        uri: "mongodb+srv://wenwettel:Quijano_23@basejgcosmiatra.lpm3v.mongodb.net/strapi?retryWrites=true&w=majority",
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
